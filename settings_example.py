2d89e0ba712d44025456d2ed824e39b580d7676a
Chris Woo <krisdestruction@gmail.com>
2017-03-13 10:09:45 -0700

    Update settings_example.py

--- settings_example.py
+++ settings_example.py
@@ -1,4 +1,5 @@
 PAGERDUTY_SUBDOMAIN = 'yoursubdomain'
+# Use the pagerduty V1 token
 PAGERDUTY_API_TOKEN = 'yourapitoken'
 ESCALATION_POLICIES = ['yourescalationpolicy']
 TIME_WINDOW_SECONDS = 7 * 24 * 60 * 60  # 1 week
